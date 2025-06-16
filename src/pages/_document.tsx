1cca4440eecc91b591c7fcbde8259c14ae19b217
Yauheni Zeliankou <zelenkovzhenya@gmail.com>
2025-06-16 16:35:23 +0300

    [info-cars-web] : added additional favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,9 @@ export default function Document() {
       <Head>
         <meta charSet="UTF-8" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-48x48.png" />
       </Head>
       <body>
         <Main />
