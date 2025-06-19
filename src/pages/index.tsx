41e8d2bb08ca7e85f82547b7036345a3651fe69d
Yauheni Zeliankou <zelenkovzhenya@gmail.com>
2025-06-19 11:51:49 +0300

    [info-cars-web] : added 'Pobit avto vin' page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import styles from "./../styles/Home.module.scss";
-import { FaInfoCircle, FaHandsHelping, FaAndroid, FaCarSide, FaVideo, FaKey } from "react-icons/fa";
+import { FaInfoCircle, FaHandsHelping, FaAndroid, FaCarSide, FaCarCrash, FaVideo, FaKey, FaCar } from "react-icons/fa";
 import { useRouter } from "next/router";
 import Chip from "@/components/common/Chip";
 import Fab from "@/components/common/Fab";
@@ -11,16 +11,20 @@ const inter = Inter({ subsets: ["latin"] });
 
 const PAGES = [
   { title: "car history (VIN)", icon: <FaCarSide />, path: "/proverka-istorii-po-vin", describtion: "Проверка истории автомобиля по VIN" },
-  { title: "car history (NUM)", icon: <FaCarSide />, path: "/proverka-avto-po-nomeru", describtion: "Проверка авто по номеру" },
-  { title: "video", icon: <FaVideo />, path: "/video", describtion: "Видео иструкции по пользованию приложением" },
+  { title: "car history (NUM)", icon: <FaCar />, path: "/proverka-avto-po-nomeru", describtion: "Проверка авто по номеру" },
+  { title: "car check history (VIN)", icon: <FaCarCrash />, path: "/probit-avto-vin", describtion: "Пробить авто по VIN" },
+  { title: "videos", icon: <FaVideo />, path: "/video", describtion: "Видео иструкции по пользованию приложением" },
   { title: "about", icon: <FaInfoCircle />, path: "/about", describtion: "Информация о сервисе" },
   { title: "support us", icon: <FaHandsHelping />, path: "/support", describtion: "Возможность поблагодарить" },
   { title: "privacy policy", icon: <FaKey />, path: "/policy", describtion: "Конфиденциальность" },
 ];
 
 const HeaderProps = {
-  title: "Онлайн проверка авто — по VIN и номеру",
-  description: "Проверка авто онлайн: узнай историю автомобиля по VIN и номеру. Быстро, надёжно, без скрытых данных.",
+  title: "Проверка автомобиля онлайн — история по VIN и госномеру",
+  description:
+    "Узнайте всё об автомобиле перед покупкой: история по VIN и номеру, ДТП, пробег, владельцы, ограничения. Бесплатная проверка с расширенным отчётом.",
+  keywords:
+    "проверка автомобиля онлайн, история авто, проверить машину по vin, проверка по номеру авто, проверка машины перед покупкой, проверка авто бесплатно",
 };
 
 export default function Home() {
@@ -35,7 +39,7 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.hero}>
           <div>
-            <h1>Проверка автомобиля по VIN и номеру</h1>
+            <h1>Проверка автомобиля онлайн — история по VIN и номеру</h1>
             <h2>Info4Cars - сервис по поиску полезной информации об авто.</h2>
           </div>
         </div>
